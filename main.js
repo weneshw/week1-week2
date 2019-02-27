0a029d52246924350033e0d4d997b1303deed3e9
weneshw <weneshwolde@gmail.com>
2019-02-26 19:33:35 -0500

    lab-day-2

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let question1 = prompt('Is Wenesh my name?')
+let question2 = prompt('Is hidden Figures my favorite movie?')
+let question3 = prompt('Am I a teacher?')
+let question4 = prompt('My birth day is end of the month')
+let question5 =prompt('Today is NewYear')
+
+if(question1 ==='Wenesh'){
+confirm('yes')
+}
+  else{
+    console.log('No')
+  }
+if(question2 ==='Is hidden Figures my favorite movie?'){
+  console.log('yes')
+}
+
+ else{console.log('No')
+}
+
+
+  if(question4 < 'Feb 20'){
+  console.log('No')
+ }
+    else{console.log('Yes')
+
+    }
