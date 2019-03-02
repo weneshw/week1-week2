2349309629293af82c68af677b8c7427295cafe5
weneshw <weneshwolde@gmail.com>
2019-03-02 14:44:18 -0500

    created table

--- demo3.js
+++ demo3.js
@@ -1,5 +1,9 @@
 const listOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-let olEl = document.createElement('ol')
+
+const listOfStudent =['Mina Ford','Sara Chase','Anderson Jeff','Dan jack','Smith Bill']
+let olEl = document = listOfStudent
+//let olEl = document.createElement('ol')
+const divEl = document.createElement(divEl)
 //let divEl = document.createEvent('div')
 const divEl =document.getElementById('listOfweek')
 
@@ -10,5 +14,15 @@ let liEl = document.createElement('li')
 liEl.textContent = `${listOfWeek[i]}`
 console.log(`${listOfWeek[i]}`)
 olEl.appendChild(liEl)
+
+ divEl.appendChild(olEl)
+
 }
+for(let i = 0; i < listOfStudent.lengtht; i++)
+{
+let liEl = document.createElement('li')
+liEl.textContent - `${listOfStudent.lengtht[i]}`
+console.log(`listOfStudet[i]}`)
+olEl.appendChild(liEl)
 divEl.appendChild(olEl)
+}
